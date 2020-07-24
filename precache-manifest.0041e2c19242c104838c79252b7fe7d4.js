0d6affc0c81f4be3de27da9cc6ba0064d03d95a3
Roland Đorđević <roland.djordjevic@ineor.si>
2020-07-24 23:42:05 +0200

    Updates

---
+++ precache-manifest.0041e2c19242c104838c79252b7fe7d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7f05d782c91d6fb907ad907c3ff0ba",
+    "url": "/tag-groups-dnd/index.html"
+  },
+  {
+    "revision": "7a34a1b18ea5b9b7ae9b",
+    "url": "/tag-groups-dnd/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "290af6beb054d747b560",
+    "url": "/tag-groups-dnd/static/js/2.e3bdbb97.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tag-groups-dnd/static/js/2.e3bdbb97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a34a1b18ea5b9b7ae9b",
+    "url": "/tag-groups-dnd/static/js/main.30d84ba5.chunk.js"
+  },
+  {
+    "revision": "a25713fc75737082361b",
+    "url": "/tag-groups-dnd/static/js/runtime-main.ebbae526.js"
+  }
+]);
