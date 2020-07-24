5bf01a326c937549b9f1220e31ffd38c6c297e46
Roland Đorđević <roland.djordjevic@ineor.si>
2020-07-24 23:35:39 +0200

    build folder

---
+++ build/precache-manifest.8d76c36ab68e10a80c3c99dcd534a354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6247cefa2acc66db125197a0144a60e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a34a1b18ea5b9b7ae9b",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "32c48095ec4132285c53",
+    "url": "/static/js/2.c51d0d62.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c51d0d62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a34a1b18ea5b9b7ae9b",
+    "url": "/static/js/main.30d84ba5.chunk.js"
+  },
+  {
+    "revision": "9e8a0e60cc253ee7a9b9",
+    "url": "/static/js/runtime-main.5f2ad8ec.js"
+  }
+]);
